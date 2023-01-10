75bd2de8e74d35799aa40c9dc4fc1dcc7bca844a
ygordeoliveira <ygor90928@gmail.com>
2023-01-10 15:08:57 -0300

    Solve exercise understanding undefined value returned from a function

---
+++ basic-javascript/understanding-undefined-value-returned-from-a-function.js
@@ -0,0 +1,16 @@
+// Setup
+let sum = 0;
+
+// Only change code below this line
+function addThree() {
+  sum = sum + 3;
+  return sum;
+}
+
+function addFive() {
+  sum += 5;
+}
+// Only change code above this line
+
+addThree();
+addFive();
