027b1414fc55898a900cf2c49c312e61e90d1d28
ygordeoliveira <ygor90928@gmail.com>
2023-01-11 15:21:39 -0300

    Solve exercise introducing else statements

---
+++ basic-javascript/introducing-else-statements.js
@@ -0,0 +1,18 @@
+function testElse(val) {
+  // Only change code below this line
+  let result = "";
+  
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+     result = "5 or Smaller"
+  }
+  return result;
+}
+
+testElse(4);
+testElse(5);
+testElse(6);
+testElse(10);
+
+//Only change code above this line
