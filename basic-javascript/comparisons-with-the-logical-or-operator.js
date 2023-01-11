0fed7ed0a82da067bf3ba7962e15093ed11cc046
ygordeoliveira <ygor90928@gmail.com>
2023-01-11 15:02:45 -0300

    Solve exercise comparisons with the logical or operator

---
+++ basic-javascript/comparisons-with-the-logical-or-operator.js
@@ -0,0 +1,17 @@
+function testLogicalOr(val) {
+  // Only change code below this line
+ if (val > 20 || val < 10) {
+    return "Outside";
+  }
+  return "Inside";
+}
+  
+testLogicalOr(0);
+testLogicalOr(9);
+testLogicalOr(10);
+testLogicalOr(15);
+testLogicalOr(19);
+testLogicalOr(20);
+testLogicalOr(21);
+testLogicalOr(25);
+// Only change code above this line
