840ee3e26ba3a6566931cc69f8b1f4e9d1657340
ygordeoliveira <ygor90928@gmail.com>
2023-01-12 12:47:52 -0300

    Solve exercise selecting from many options with switch statements

---
+++ basic-javascript/selecting-from-many-options-with-switch-statements.js
@@ -0,0 +1,22 @@
+function caseInSwitch(val) {
+// Only change code below this line
+
+  switch (val) {
+    case 1: 
+      return "alpha";
+      break;
+    case 2: 
+      return "beta";
+      break;
+    case 3:
+      return "gamma";
+    case 4: 
+      return "delta";
+      break;
+  }
+}
+caseInSwitch(1);
+caseInSwitch(2);
+caseInSwitch(3);
+caseInSwitch(4);
+// Only change code above this line
