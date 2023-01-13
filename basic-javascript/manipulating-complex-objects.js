522d1603beef624c1ab5dd854e5df927b33ec9ee
ygordeoliveira <ygor90928@gmail.com>
2023-01-13 14:42:16 -0300

    Solve exercise manipulating complex objects

---
+++ basic-javascript/manipulating-complex-objects.js
@@ -0,0 +1,21 @@
+const myMusic = [ 
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T"
+    ],
+  },
+
+  {
+   "artist": "Taylor Swift",
+   "title": "Singer",
+   "release_year": 2006,
+   "formats": [
+     "Artista Revelação",
+     "Artista do Ano"
+  ],
+ }
+];
