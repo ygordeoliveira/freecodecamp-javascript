c827dc52c1a2481c22a6cd6f8b1cdc33b39a095a
ygordeoliveira <ygor90928@gmail.com>
2023-01-14 22:19:23 -0300

    Solve exercise use the parseint function with a radix

---
+++ basic-javascript/use-the-parseint-function-with-a-radix.js
@@ -0,0 +1,9 @@
+function convertToInteger(str) {
+   const conversion = parseInt(str,2);
+   return conversion;
+}
+
+convertToInteger("10011");
+convertToInteger("10011");
+convertToInteger("111001");
+convertToInteger("JamesBond");
