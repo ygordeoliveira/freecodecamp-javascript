adf029ee4422c130b1c94f35a868130e95e5f911
ygordeoliveira <ygor90928@gmail.com>
2023-01-17 13:35:45 -0300

    I changed the return spaces

--- basic-javascript/nesting-for-loops.js
+++ basic-javascript/nesting-for-loops.js
@@ -1,15 +1,15 @@
 function multiplyAll(arr) {
   let product = 1;
+  
   // Only change code below this line
  for (let i = 0; i < arr.length; i++) {
    for (let j = 0 ; j < arr[i].length; j++) {
     product *= arr[i][j];
    }
  }
-  return product;
+ return product;
 }
 multiplyAll([[1], [2], [3]]);
 multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
 multiplyAll([[5, 1], [0.2, 4, 0.5], [3, 9]]);
-
-  // Only change code above this line
+// Only change code above this line
