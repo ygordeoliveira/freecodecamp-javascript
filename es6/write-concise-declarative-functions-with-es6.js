9931f9f83986233677b566e405d0b5a3f3beed47
ygordeoliveira <ygor90928@gmail.com>
2023-01-19 11:34:00 -0300

    Solve exercise write concise declarative functions with es6

---
+++ es6/write-concise-declarative-functions-with-es6.js
@@ -0,0 +1,11 @@
+// Only change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    this.gear = newGear;
+  }
+};
+
+bicycle.setGear(48);
+console.log(bicycle.gear);
+// Only change code above this line
