7c776d36338331056fd5f9d9a229192216a1de30
ygordeoliveira <ygor90928@gmail.com>
2023-01-24 11:13:11 -0300

    I changed the name given to the constructor parameter and its attribute

--- es6/use-class-syntax-to-define-a-constructor-function.js
+++ es6/use-class-syntax-to-define-a-constructor-function.js
@@ -1,16 +1,13 @@
 // Only change code below this line
 class Vegetable {
-  constructor(carrot) {
-    this.name = carrot;
+  constructor(name) {
+    this.name = name;
   }
   showCarrot() {
     return "carrot" 
   }  
 }
-// Only change code above this line
 
 const carrot = new Vegetable('carrot');
 console.log(carrot.name); // Should display 'carrot'
-
-
-//A classe Vegetable permite criar um objeto vegetal com um nome de propriedade que é passado para o construtor.
+// Only change code above this line
