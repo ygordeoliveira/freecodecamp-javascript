57342b88907fd301562d307d8ac7d9fd8f8643d2
ygordeoliveira <ygor90928@gmail.com>
2023-01-27 13:51:22 -0300

    Solve exercise check for mixed grouping of characters

---
+++ regular-expressions/check-for-mixed-grouping-of-characters.js
@@ -0,0 +1,3 @@
+let myString = "Eleanor Roosevelt";
+let myRegex = /(Eleanor|Franklin D.)\sRoosevelt/gi;     //Change this line
+let result = myRegex.test(myString);                   //Change this line
