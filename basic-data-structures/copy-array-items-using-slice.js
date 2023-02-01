f0ced90acd04b05d03ccbc36c8f0cf4cb1129a68
ygordeoliveira <ygor90928@gmail.com>
2023-02-01 14:30:43 -0300

    Solve exercise copy array items using slice

---
+++ basic-data-structures/copy-array-items-using-slice.js
@@ -0,0 +1,8 @@
+function forecast(arr) {
+  // Only change code below this line
+  let newArray = arr.slice(2, 4);
+  return newArray;
+ // Only change code above this line
+}
+
+forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']);
