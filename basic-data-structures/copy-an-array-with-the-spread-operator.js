052a8141eed1f239c53bbf037af7542e904dafe1
ygordeoliveira <ygor90928@gmail.com>
2023-02-04 16:14:13 -0300

    I used another loop in the exercise to show other ways to do the exercise

--- basic-data-structures/copy-an-array-with-the-spread-operator.js
+++ basic-data-structures/copy-an-array-with-the-spread-operator.js
@@ -1,14 +1,13 @@
 function copyMachine(arr, num) {
-  let newArr = [];
-  // Only change code below this line
-  for (let i = 0; i < arr.length; i++) {
-   newArr = [];
+    let newArr = [];
+
+    while (num >= 1) {
+    // Only change code below this line
+     newArr.push([...arr]);
+    // Only change code above this line
+      num--;
+    }
+    return newArr;
   }
-  for(let j = 0; j < num; j++) {
-   newArr.push([...arr]);
-  }
-  return newArr;
-}
-// Only change code above this line
 
 copyMachine([true, false, true], 2);
