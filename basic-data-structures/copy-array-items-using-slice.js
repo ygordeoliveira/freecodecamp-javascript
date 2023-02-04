4690360b9adf10ec5e6793557bc30aea0f270416
ygordeoliveira <ygor90928@gmail.com>
2023-02-04 16:20:42 -0300

    I made the return statement directly for the sliced array

--- basic-data-structures/copy-array-items-using-slice.js
+++ basic-data-structures/copy-array-items-using-slice.js
@@ -1,7 +1,6 @@
 function forecast(arr) {
   // Only change code below this line
-  let newArray = arr.slice(2, 4);
-  return newArray;
+  return arr.slice(2, 4);
  // Only change code above this line
 }
 
