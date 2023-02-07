f2c18a17eb4b5112e2474c47dfdb95c967ec7ace
ygordeoliveira <ygor90928@gmail.com>
2023-02-07 18:04:10 -0300

    Solve exercise return largest numbers in arrays

---
+++ basic-algorithm-scripting/return-largest-numbers-in-arrays.js
@@ -0,0 +1,17 @@
+function largestOfFour(arr) {
+  let arrMax = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let max = arr[i][0];
+
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] > max) {
+        max = arr[i][j];
+      }
+    }
+    arrMax.push(max);
+  }
+  return arrMax;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
