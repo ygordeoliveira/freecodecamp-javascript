68d47816ecf18a013f0b9f0a0d298ef4deefc2a1
ygordeoliveira <ygor90928@gmail.com>
2023-02-10 17:01:24 -0300

    I used the for...of loop to find the longest word in a string

--- basic-algorithm-scripting/find-the-longest-word-in-a-string.js
+++ basic-algorithm-scripting/find-the-longest-word-in-a-string.js
@@ -1,13 +1,13 @@
 function findLongestWordLength(str) {
-    let words = str.split(" ");
-    let longestWordLength = 0;
-    
-    for (let word = 0; word < words.length; word++) {
-      if (words[word].length > longestWordLength) {
-        longestWordLength = words[word].length;
-      }
+  let words = str.split(" ");
+  let longestWordLength = 0;
+  
+  for (let word of words) {
+    if (word.length > longestWordLength) {
+      longestWordLength = word.length;
     }
-    return longestWordLength;
   }
+  return longestWordLength;
+}
 
 findLongestWordLength("The quick brown fox jumped over the lazy dog");
