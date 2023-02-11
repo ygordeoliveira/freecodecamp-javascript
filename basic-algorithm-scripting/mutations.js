bee2fd6cb20c60a710963e4ce3f639b3ecba74a6
ygordeoliveira <ygor90928@gmail.com>
2023-02-11 10:15:46 -0300

    I used the includes method and used the negative operator in front of the if statement expression

--- basic-algorithm-scripting/mutations.js
+++ basic-algorithm-scripting/mutations.js
@@ -3,7 +3,7 @@ function mutation(arr) {
   let secondWord = arr[1].toLowerCase();
 
   for (let i = 0; i < secondWord.length; i++) {
-    if (firstWord.indexOf(secondWord[i]) === -1) {
+    if (!(firstWord.includes(secondWord[i]))) {
       return false;
     }
   }
