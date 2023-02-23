08b4e9e9da1ea02e14081950886c5a181dface7a
ygordeoliveira <ygor90928@gmail.com>
2023-02-23 18:15:45 -0300

    Solve exercise implement the filter method on a prototype

---
+++ functional-programming/implement-the-filter-method-on-a-prototype.js
@@ -0,0 +1,11 @@
+Array.prototype.myFilter = function(callback) {
+   const newArray = [];
+  // Only change code below this line
+   for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      newArray.push(this[i]);
+    }
+  }
+  return newArray;
+};
+// Only change code above this line
