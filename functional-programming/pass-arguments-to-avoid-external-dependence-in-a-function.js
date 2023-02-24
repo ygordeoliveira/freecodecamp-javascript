4a630352bef1b1194b82594cb030928612b9500d
ygordeoliveira <ygor90928@gmail.com>
2023-02-24 16:40:40 -0300

    I used the space character in the code within the function and put the last comment out of the function

--- functional-programming/pass-arguments-to-avoid-external-dependence-in-a-function.js
+++ functional-programming/pass-arguments-to-avoid-external-dependence-in-a-function.js
@@ -3,6 +3,6 @@ let fixedValue = 4;
 
 // Only change code below this line
 function incrementer(fixedValue) {
-  return fixedValue + 1;
-// Only change code above this line
+    return fixedValue + 1;
 }
+// Only change code above this line
