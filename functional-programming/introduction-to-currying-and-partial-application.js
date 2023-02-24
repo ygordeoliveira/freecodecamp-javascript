c228401fd4f9a052e2aeebaf91954d13085de102
ygordeoliveira <ygor90928@gmail.com>
2023-02-24 15:49:28 -0300

    I used the space character in the code blocks inside the function and aligned them with the bracket closeds of each block

--- functional-programming/introduction-to-currying-and-partial-application.js
+++ functional-programming/introduction-to-currying-and-partial-application.js
@@ -1,10 +1,10 @@
 function add(x) {
-// Only change code below this line
- return function(y) {
-   return function(z) {
-    return x + y + z;
-    } 
-  }
+    // Only change code below this line
+    return function(y) {
+        return function(z) {
+        return x + y + z;
+        } 
+    }
 }
 // Only change code above this line
 
