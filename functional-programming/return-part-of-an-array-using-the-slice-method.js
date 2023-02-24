8b5ff03569f36fa225774c7b408a1a76d749e03f
ygordeoliveira <ygor90928@gmail.com>
2023-02-24 17:04:10 -0300

    I used the space character in the return declaration and in the function comments

--- functional-programming/return-part-of-an-array-using-the-slice-method.js
+++ functional-programming/return-part-of-an-array-using-the-slice-method.js
@@ -1,7 +1,7 @@
 function sliceArray(anim, beginSlice, endSlice) {
- // Only change code below this line
-  return anim.slice(beginSlice, endSlice);
-// Only change code above this line
+    // Only change code below this line
+    return anim.slice(beginSlice, endSlice);
+    // Only change code above this line
 }
 
 const inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
