8641d081925f19c68c0da2c3f829ea388814dd8c
ygordeoliveira <ygor90928@gmail.com>
2023-02-24 15:26:06 -0300

    I used the space character in the code from within the function

--- functional-programming/combine-an-array-into-a-string-using-the-join-method.js
+++ functional-programming/combine-an-array-into-a-string-using-the-join-method.js
@@ -1,8 +1,8 @@
 function sentensify(str) {
-// Only change code below this line
+    // Only change code below this line
     let words = str.split(/\W/);
     return words.join(" ");
-// Only change code above this line
+    // Only change code above this line
 };
 
 sentensify("May-the-force-be-with-you");
