14147a5fcfe3e43e139670874d76fe26c79ecbe3
ygordeoliveira <ygor90928@gmail.com>
2023-02-24 15:41:45 -0300

    I used the space character in the code blocks from within the function

--- functional-programming/implement-the-filter-method-on-a-prototype.js
+++ functional-programming/implement-the-filter-method-on-a-prototype.js
@@ -1,11 +1,11 @@
 Array.prototype.myFilter = function(callback) {
-   const newArray = [];
-  // Only change code below this line
-   for (let i = 0; i < this.length; i++) {
-    if (callback(this[i], i, this)) {
-      newArray.push(this[i]);
+    const newArray = [];
+    // Only change code below this line
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+        newArray.push(this[i]);
+       }
     }
-  }
-  return newArray;
+    return newArray;
 };
 // Only change code above this line
