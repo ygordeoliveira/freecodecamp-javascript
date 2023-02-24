59fd940a4ec20883c1ad9c309d72d2b47d9d604e
ygordeoliveira <ygor90928@gmail.com>
2023-02-24 15:17:29 -0300

    I used the code space character within function

--- functional-programming/apply-functional-programming-to-convert-strings-to-url-slugs.js
+++ functional-programming/apply-functional-programming-to-convert-strings-to-url-slugs.js
@@ -1,6 +1,7 @@
-// Only change code below this line
 function urlSlug(title) {
-  return title.trim().split(/\s+/).join("-").toLowerCase();
+    // Only change code below this line
+    return title.trim().split(/\s+/).join("-").toLowerCase();
+    // Only change code above this line
 }
-// Only change code above this line
+
 urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone");
