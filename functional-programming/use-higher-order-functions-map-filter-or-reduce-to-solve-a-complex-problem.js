f2b84736a6dc7d6fd0cc6f95626f07976508d435
ygordeoliveira <ygor90928@gmail.com>
2023-02-24 17:47:38 -0300

    I used the space character in the code blocks of the functions and changed the place of the last comment

--- functional-programming/use-higher-order-functions-map-filter-or-reduce-to-solve-a-complex-problem.js
+++ functional-programming/use-higher-order-functions-map-filter-or-reduce-to-solve-a-complex-problem.js
@@ -1,15 +1,15 @@
 const squareList = arr => {
-// Only change code below this line
-   const positiveIntegers = arr.filter(num => {
-    return num >= 0 && Number.isInteger(num);
-   });
+    // Only change code below this line
+    const positiveIntegers = arr.filter(num => {
+        return num >= 0 && Number.isInteger(num);
+    })
 
-  const squaredIntegers = positiveIntegers.map(num => {
-    return num ** 2;
-  });
-   return squaredIntegers;
+    const squaredIntegers = positiveIntegers.map(num => {
+        return num ** 2;
+    })
+    return squaredIntegers;
+    // Only change code above this line
 };
-// Only change code above this line
 
 const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
 console.log(squaredIntegers);
