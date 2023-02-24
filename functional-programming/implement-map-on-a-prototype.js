fedb8944346f734c96c079eb2709bcf5fd83016e
ygordeoliveira <ygor90928@gmail.com>
2023-02-24 15:35:17 -0300

    I used the space character in the code block within the function

--- functional-programming/implement-map-on-a-prototype.js
+++ functional-programming/implement-map-on-a-prototype.js
@@ -1,9 +1,9 @@
 Array.prototype.myMap = function(callback) {
-  const newArray = [];
-  // Only change code below this line
-  for (let i = 0; i < this.length; i++) {
-    newArray.push(callback(this[i], i, this))
-  }
-  // Only change code above this line
-  return newArray;
+    const newArray = [];
+    // Only change code below this line
+    for (let i = 0; i < this.length; i++) {
+        newArray.push(callback(this[i], i, this));
+    }
+    // Only change code above this line
+    return newArray;
 };
