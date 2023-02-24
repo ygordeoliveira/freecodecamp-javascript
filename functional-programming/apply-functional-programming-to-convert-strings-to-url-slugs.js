ca908d1b3ca6ac5adbe7355129479faefcb4b9b6
ygordeoliveira <ygor90928@gmail.com>
2023-02-24 11:20:27 -0300

    Solve exercise apply functional programming to convert strings to url slugs

---
+++ functional-programming/apply-functional-programming-to-convert-strings-to-url-slugs.js
@@ -0,0 +1,6 @@
+// Only change code below this line
+function urlSlug(title) {
+  return title.trim().split(/\s+/).join("-").toLowerCase();
+}
+// Only change code above this line
+urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone");
