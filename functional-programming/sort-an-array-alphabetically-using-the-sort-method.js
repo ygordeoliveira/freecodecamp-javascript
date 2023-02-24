a09578287b0d6fb8b070721e542b4aebb39719d8
ygordeoliveira <ygor90928@gmail.com>
2023-02-24 09:53:29 -0300

    Solve exercise sort an array alphabetically using the sort method

---
+++ functional-programming/sort-an-array-alphabetically-using-the-sort-method.js
@@ -0,0 +1,10 @@
+function alphabeticalOrder(arr) {
+// Only change code below this line
+  let arrayOrdered = arr.sort(function(a, b) {
+    return a === b ? 0 : a < b ? 1 : -1;
+  });
+  return arrayOrdered.reverse();
+}
+// Only change code above this line
+
+alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);
