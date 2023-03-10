a33f4b11e4fafac53cb4a359ad4f92fd477bcab7
ygordeoliveira <ygor90928@gmail.com>
2023-03-10 16:44:29 -0300

    I changed the variable names, used the for..of loop and the includes method

--- intermediate-algorithm-scripting/seek-and-destroy.js
+++ intermediate-algorithm-scripting/seek-and-destroy.js
@@ -1,14 +1,14 @@
 function destroyer(arr) {
-    let survivingElements = [];
-    let battleField = Object.values(arguments)[0];
-    let elementsToDestroy = Object.values(arguments).splice(1);
-        
-    battleField.forEach(positionOfElements => {
-        if (elementsToDestroy.indexOf(positionOfElements) === -1) {
-            survivingElements.push(positionOfElements);
+    let remainingElements = [];
+    let originalElements = Object.values(arguments)[0];
+    let elementsToRemove = Object.values(arguments).splice(1); 
+
+    for (let elements of originalElements) {
+        if (!elementsToRemove.includes(elements)) {
+            remainingElements.push(elements);
         }
-    });
-    return survivingElements;
+    }
+    return remainingElements; 
 }
 
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
