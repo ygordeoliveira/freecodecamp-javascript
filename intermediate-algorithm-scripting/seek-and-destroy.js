e606c1a8c026900449ac0c009f554243db4fcca7
ygordeoliveira <ygor90928@gmail.com>
2023-03-15 10:45:04 -0300

    I used the forEach method to loop through each element of the original array

--- intermediate-algorithm-scripting/seek-and-destroy.js
+++ intermediate-algorithm-scripting/seek-and-destroy.js
@@ -1,14 +1,15 @@
 function destroyer(arr) {
     let remainingElements = [];
     let originalElements = Object.values(arguments)[0];
-    let elementsToRemove = Object.values(arguments).splice(1); 
-
-    for (let elements of originalElements) {
+    let elementsToRemove = Object.values(arguments).splice(1);
+  
+    
+    originalElements.forEach(elements => {
         if (!elementsToRemove.includes(elements)) {
             remainingElements.push(elements);
         }
-    }
-    return remainingElements; 
-}
-
-destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+    });
+    return remainingElements;
+  }
+                          
+  destroyer([1, 2, 3, 1, 2, 3], 2, 3);
