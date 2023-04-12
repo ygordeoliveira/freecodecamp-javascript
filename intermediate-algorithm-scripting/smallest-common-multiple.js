4982d144d83370a8fdeb5ccd0150db0d41726a7d
Ygor de Oiveira Souza da Silva <ygor90928@gmail.com>
2023-04-12 13:12:34 -0300

    I removed the generator function instead I used three variables, a global variable that holds the prime numbers and the other two to keep track of the index of the array of primes and update the prime number, I created a function to compare two arrays and say if they are same or not

--- intermediate-algorithm-scripting/smallest-common-multiple.js
+++ intermediate-algorithm-scripting/smallest-common-multiple.js
@@ -1,20 +1,12 @@
+const PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23];
 function createRange(infLimit, supLimit) {
     let range = [];
-
     for (let num = infLimit; num <= supLimit; num++) {
         range.push(num);
     };
     return range;
 }
 
-function* primes() {
-    const primes = [2, 3, 5, 7, 11, 13, 17, 19, 21, 23, 29, 31, 37];
-
-    for (let currentPrime of primes) {
-        yield currentPrime;
-    };
-    return 41;
-}
 
 function onlyHaveOne(arr) {
     return arr.every((number) => {
@@ -22,38 +14,40 @@ function onlyHaveOne(arr) {
     });
 }
 
+function arraysAreEqual(baseNumbers, numbers) {
+   return baseNumbers.toString() === numbers.toString();
+}
+
 function smallestCommons(arr) {
     arr.sort((a, b) => a - b);
-
     let baseNumbers = createRange(arr[0], arr[1]);
-  
+    let primeIndex = 0;
+    let prime = PRIMES[0];
     let numbers = [];
- 
     let mmc = 1; 
-    let getPrime = primes();
-    let prime = getPrime.next().value;
 
     while (true) {
         if (onlyHaveOne(baseNumbers)) {
             break;
         }
 
-    numbers = baseNumbers.map(number => {
-        if (number % prime === 0) {
-            return number / prime;
-        }
-        return number;
-    });
+        numbers = baseNumbers.map(number => {
+            if (number % prime === 0) {
+                return number / prime;
+            }
+            return number;
+       });
 
-    if (JSON.stringify(baseNumbers) === JSON.stringify(numbers)) {
-        prime = getPrime.next().value;
-    }
-    else {
-        mmc *= prime;
-        baseNumbers = numbers;
-    }
-  }
-  return mmc;
+        if (arraysAreEqual(baseNumbers, numbers)) {
+            primeIndex++;
+            prime = PRIMES[primeIndex];
+        }
+        else {
+            mmc *= prime;
+            baseNumbers = numbers;
+        }
+   }
+   return mmc;
 }
 
-smallestCommons([1, 5]);
+smallestCommons([2, 10]);
